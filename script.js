c5a5782d089709fde1769e2aa2e7fb0b9ad334f7
ericaribeirx <ericaribeirx@gmail.com>
2026-04-15 13:54:14 -0300

    Projeto finalizado com HTML, CSS e JavaScript

---
+++ script.js
@@ -0,0 +1,52 @@
+const formulario = document.querySelector('#meu-formulario');
+
+function validarFormulario(event) {
+    event.preventDefault();
+
+     const nome = document.querySelector('#nome').value;
+     const email = document.querySelector('#email').value;
+     const msg = document.querySelector('#msg').value;
+
+     if (nome === "" || email === "" || msg === "") {
+        alert("Por favor, preencha todos os campos antes de enviar.");
+     } else {
+        alert(`Obrigada, ${nome}! Sua mensagem foi enviada com sucesso.`);
+        formulario.reset();
+     }
+}
+
+formulario.addEventListener('submit', validarFormulario);
+
+const meusProjetos = [
+   {
+      titulo: "Portfólio",
+      descricao: "Meu portfólio de apresentação feito com HTML, CSS e JavaScript.",
+      link: "https://github.com/ericaribeirx",
+   },
+
+    {
+      titulo: "Projeto 01",
+      descricao: "Descrição",
+      link: "https://github.com/ericaribeirx",
+   }
+];
+
+function renderizarProjetos() {
+   const container = document.querySelector('.projetos-container');
+
+   container.innerHTML = "";
+
+   meusProjetos.forEach(projeto => {
+      const cardHTML = `
+      <article class="card-projeto">
+         <h3>${projeto.titulo}</h3>
+         <p>${projeto.descricao}</p>
+         <a href="${projeto.link}" target="_blank"> Ver no GitHub</a>
+      </article>
+      `;
+      container.innerHTML += cardHTML;
+
+   });
+}
+
+renderizarProjetos();
